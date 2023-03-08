225d5cd559d86cb3ca623598a94c4114cba8bde3
YoungSi <jhonjairoramos2000@gmail.com>
2023-03-08 14:51:14 -0600

    se agregan operadores

--- repasoPython/main.py
+++ repasoPython/main.py
@@ -25,3 +25,47 @@ print(esColombiano)
 
 mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida\\'"
 print(mensaje)
+
+# Operadores
+# Arimeticos
+numero5 = 9
+numero6 = 12
+suma = numero5 + numero6
+multiplicacion = numero6 * numero5
+resta = numero5 - numero6
+division = numero5 / numero6
+modulo = numero5 % numero6
+print("la suma es: ",suma)
+print("La multiplicacion es :",multiplicacion)
+print("La resta es :", resta)
+print("La dvision es :", division)
+print("El modulo es:", modulo)
+
+# Asignacion
+x = 7
+y = 8
+z = 2
+print(x)
+
+# Logicos
+# and(y)
+
+q = 5
+
+print(q > 4 and q < 9)
+
+# or(o)
+p = 4
+print(not(p > 5 or p < 7))
+
+# Relacioales
+
+valor1 = 7
+valor2 = 9
+
+print(valor1 == valor2) # Igualdad
+print(valor1 > valor2) # Mayor que
+print(valor1 < valor2) # Menor que
+print(valor1 >= valor2) # Menor igual que
+print(valor1 <= valor2) # Menor igual que
+print(valor1 != valor2) # No igual
